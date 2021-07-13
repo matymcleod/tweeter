41a9d7e2ffe848b14d34e7015d7f18baa51e59b8
matymcleod <matymcleod@gmail.com>
2021-07-13 23:44:35 +0000

    changing counter to update char count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-  console.log("We are connected to the js file and ready to use jQuery");
+  console.log('We are connected to the js file and ready to use jQuery');
 });
 
-$("#text-input").on('blur', function() {
-  console.log(this);
+let counter = $('#counter');
+
+$('#text-input').on('input', function() {
+  let currentLength = $(this).val().length;
+  counter.val(140 - currentLength);
+  if(currentLength >= 140) {
+    counter.css('color', 'red');
+  } else {
+    counter.css('color', 'black');
+  }
 });
 
 
