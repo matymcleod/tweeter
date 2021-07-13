8c0badf2f00ce650bf24fdba24011a7a7d713aac
matymcleod <matymcleod@gmail.com>
2021-07-13 22:34:46 +0000

    changing name of textarea id to text-input, testing of char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
   console.log("We are connected to the js file and ready to use jQuery");
 });
 
-$("#tweet-text").on('blur', function() {
+$("#text-input").on('blur', function() {
   console.log(this);
 });
+
+
+
+
+
+
+
+
